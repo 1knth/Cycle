2735e712fa28fe50e058f63831753202c68a8601
Kenneth <knthyang@gmail.com>
2026-01-23 05:35:38 -0500

    text-align hero title

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
+//middleware
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 
 //Mongodb config
@@ -23,8 +30,4 @@ app.get('/api/transactions/add', (req, res) => {
 });
 
 require('./routes/transactionRoutes.js')(app);
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
 
